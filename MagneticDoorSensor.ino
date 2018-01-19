8c7ac17544dceeb9c3d310dcf396d8281efd6acf
moeyehtet96 <33075436+moeyehtet96@users.noreply.github.com>
2018-01-19 00:12:38 -0500

    Arduino Sketch
    Upload the Arduino sketch for the magnetic door sensor

---
+++ MagneticDoorSensor.ino
@@ -0,0 +1,57 @@
+//Declare Pins for Magnetic Sensor and Buzzer/LED
+const int magSwitch = 8;
+const int buzz = 9;
+const int trig = 11;
+const int echo = 12;
+
+long duration;
+float distance;
+
+void setup() {
+  // Set sensor as input and buzzer/LED as output
+  pinMode(magSwitch,INPUT);
+  pinMode(buzz,OUTPUT);
+  // Set sensor pin to high
+  digitalWrite(magSwitch,HIGH);
+  // Set trig as output and echo as input
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Clears trig pin
+  digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+
+  // Set trig to HIGH for 10 microseconds
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+
+  // find duration and distance
+  duration = pulseIn(echo,HIGH);
+  distance = duration*0.034/2;
+
+  // Prints duration and distance
+  Serial.print("Duration: ");
+  Serial.println(duration);
+  //Serial.print("Distance: ");
+  //Serial.println(distance);
+  
+  if (digitalRead(magSwitch) == LOW)
+  {
+    digitalWrite(buzz,LOW);
+  }
+  else if ((digitalRead(magSwitch) == HIGH) && (distance <= 100))
+  {
+    digitalWrite(buzz,HIGH);
+  }
+  else
+  {
+    digitalWrite(buzz,HIGH);
+    delay(100);
+    digitalWrite(buzz,LOW);
+    delay(100);
+  }
+
+}
